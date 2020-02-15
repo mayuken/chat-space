ef807c5ccd3f2c3ef995e010d8b718d897f821b3
mayuken <superbeaver.drum@gmail.com>
2020-02-15 10:16:28 +0900

    automatic_updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,42 +1,77 @@
 $(function(){
 
+  var reloadMessages = function() {
+    last_message_id = $('.message__list:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message--list').append(insertHTML);
+        $('.chat-main__message--list').animate({ scrollTop: $('.chat-main__message--list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert("自動更新に失敗しました");
+    });
+  };
+
   function buildHTML(message){
-    if (message.image) {
-      var html =`<div class="message__list">
-                  <ul class="message__info">
-                    <li class="user__name">
-                      ${message.user_name}
-                    </li>
-                    <li class="date">
-                      ${message.created_at}
-                    </li>
-                    </ul>
-                    <p class="message">
-                    </p><p class="lower-message__content">
-                      ${message.content}
-                    </p>
-                    <img class="lower-message__image" src="${message.image}">
-                    <p></p> 
-                </div>`
-    } 
-    else {
-      var html = `<div class="message__list">
-                    <ul class="message__info">
-                      <li class="user__name">
+    if (message.content && message.image) {
+      var html = `<div class="message__list" data-message-id=${message.id}>
+                    <div class="message__info">
+                      <div class="user__name">
                         ${message.user_name}
-                      </li>
-                      <li class="date">
+                      </div>
+                      <div class="date">
                         ${message.created_at}
-                      </li>
-                      </ul>
-                      <p class="message">
-                      </p><p class="lower-message__content">
+                      </div>
+                    </div>
+                    <div class="message">
+                      <p class="lower-message__content">
                         ${message.content}
                       </p>
-                      
-                      <p></p>
+                      <img src=${message.image} class="lower-message__image" >
+                    </div>
                   </div>`
-    }
+    } else if (message.content) {
+      var html = `<div class="message__list" data-message-id=${message.id}>
+                    <div class="message__info">
+                      <div class="user__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+    } else if (message.image) {
+      var html = `<div class="message__list" data-message-id=${message.id}>
+                    <div class="message__info">
+                      <div class="user__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message">
+                      <img src=${message.image} class="lower-message__image" >
+                    </div>
+                  </div>`
+    };
     return html;
   }
 
@@ -64,4 +99,7 @@ $(function(){
       $('.send__btn').prop('disabled', false);
     });
   })
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 })
